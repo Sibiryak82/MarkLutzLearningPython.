b9fc7ce8038e7f866b04323b914c19233182f1bb
Sibiryak82 <80231116+Sibiryak82@users.noreply.github.com>
2021-06-19 05:58:28 +0800

    Add files via upload

---
+++ class C.py
@@ -0,0 +1,11 @@
+class C:
+    data = 'spam'
+    def __gt__(self, other):
+        return self.data > other
+    def __lt__(self, other):
+        return self.data < other
+X = C()
+print(X > 'ham')
+print(X < 'ham')
+
+
