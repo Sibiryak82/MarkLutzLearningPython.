1771be1c2c4d32b7447031553c806ec4a7f61411
Sibiryak82 <80231116+Sibiryak82@users.noreply.github.com>
2021-06-30 10:13:33 +0800

    Add files via upload

---
+++ classexc.py
@@ -0,0 +1,24 @@
+# Файл classexc.py
+
+class General(Exception): pass
+class Specific1(General): pass
+class Specific2(General): pass
+
+def raiser0():
+    X = General()                                 # Генерирует экземпляр суперкласса
+    raise X
+
+def raiser1():
+    X = Specific1()                               # Генерирует экземпляр подкласса
+    raise X
+
+def raiser2():
+    X = Specific2()                               # Генерирует экземпляр другого подкласса
+    raise X
+
+for func in (raiser0, raiser1, raiser2):
+    try:
+        func()
+    except General:                               # Соответсвует суперклассу General1 или любому его подклассу
+        import sys
+        print('caught: %s' % sys.exc_info()[0])
