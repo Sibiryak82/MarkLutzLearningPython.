0244ee1d17dc76abc8009e76e0e9ac485a595d0f
Sibiryak82 <80231116+Sibiryak82@users.noreply.github.com>
2021-07-07 17:03:36 +0800

    Add files via upload

--- getattr-bultins.py
+++ getattr-bultins.py
@@ -1,29 +1,29 @@
 # Файл getattr-bultins.py
 
 class GetAttr:
-    eggs = 88                                         #
+    eggs = 88                                         # eggs храниться в классе, spam - в экземпляре
     def __init__(self):
         self.spam = 77
-    def __len__(self):                                #
-                                                      #
+    def __len__(self):                                # Реализовать здесь len,
+                                                      # иначе __getattr__ вызывается с __len__
         print('__len__: 42')
         return 42
-    def __getattr__(self, attr):                      #
-                                                      #
+    def __getattr__(self, attr):                      # Предоставить __str__ по запросу,
+                                                      # иначе фиктивную функцию
         print('getattr: ' + attr)
         if attr == '__str__':
             return lambda *args: '[Getattr str]'
         else:
             return lambda *args: None
 
-class GetAttribute(object):                           #
-                                                      #
-    eggs = 88                                         #
-                                                      #
-    def __init__(self):                               #
-                                                      #
-        self.spam = 77                                #
-                                                      #
+class GetAttribute(object):                           # object требуется в Python 2.X
+                                                      # и подразумевается в Python 3.X
+    eggs = 88                                         # В Python 2.X все автоматически
+                                                      # isinstance(object)
+    def __init__(self):                               # Но нужно наследовать от object,
+                                                      # чтобы получить инструменты нового
+        self.spam = 77                                # стиля, включая __getattibute__
+                                                      # и ряд стандартных методов __X__
     def __len__(self):
         print('__len__: 42')
         return 42
